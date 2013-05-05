667f93bb6643be86220b826d74e8bdf8c4abdc01
daniel <xzdasx@gmail.com>
2013-05-04 20:09:40 -0430

    concatenar con comillas dobles

---
+++ concatenar.php
@@ -0,0 +1,14 @@
+<?php
+  $nombre1 = 'pedro';
+  $mascota1 = 'perro';
+  $precio = "20";
+  // CONCATENAR STRINGS
+  echo 'El usuario '.$nombre1.'. Compro un '.$mascota1.' por Bs. '.$precio;
+  echo "<br>";
+  
+  /*Mayor legibilidad*/
+  echo "El usuario $nombre1. Compro un $mascota1 por Bs. $precio";
+
+
+
+

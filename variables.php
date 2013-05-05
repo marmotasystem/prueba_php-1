11e64dfac1194a5c718e1d22674c7b9e85016e57
daniel <xzdasx@gmail.com>
2013-05-04 20:29:57 -0430

    Operaciones

---
+++ variables.php
@@ -0,0 +1,29 @@
+<?php
+  
+  $numero = 1;
+  echo($numero);
+  echo "<br>";
+  $flotante = 1.1;
+  echo($flotante);
+  echo "<br>";
+  $texto = 'Hola';
+  echo($texto);
+  echo "<br>";
+  $texto_2 = "$texto, chao";
+  echo($texto_2);
+  echo "<br>";
+  $texto_3 = '$texto, chao';
+  echo($texto_3);
+  echo "<br>";
+  $arreglo = array();
+  echo($arreglo);
+  echo "<br>";
+  $usuarioRegistrado = 20;
+  echo($usuarioRegistrado);echo "<br>";
+
+  /*                                     
+      comentario multilinea
+  */
+
+  // Comentario simple
+  /* Cuando usar y porque usar comillas dobles */
